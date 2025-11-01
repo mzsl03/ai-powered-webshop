9b0f860a6d2c41d09724b320fc7cf1eaaa8b38d2
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-01 16:07:02 +0100

    feat: add login page to project

--- core_site/application/views.py
+++ core_site/application/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Hibás felhasználónév vagy jelszó!'})
+    return render(request, 'login.html')
