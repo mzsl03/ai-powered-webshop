5c2cb2ee75f557e4e246fade9e946c19bf76d68c
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-04 21:36:13 +0100

    fix: update user info now makes changes in database

--- core_site/application/forms/user_update_form.py
+++ core_site/application/forms/user_update_form.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.core.exceptions import ValidationError
-
 from application.models import UserInfo
 
 
@@ -17,45 +15,33 @@ class UserUpdateForm(forms.ModelForm):
         }
     )
 
-    email = forms.EmailField(
-        label="E-mail",
-        error_messages={'invalid': 'Helytelen e-mail formátum.'}
-    )
-
-    first_name = forms.CharField(max_length=255, label="Keresztnév")
-    last_name = forms.CharField(max_length=255, label="Vezetéknév")
-
-    address = forms.CharField(max_length=255, label="Cím")
-    phone_number = forms.CharField(max_length=12, label="Telefonszám")
+    email = forms.EmailField(label="E-mail", required=True)
+    address = forms.CharField(max_length=255, label="Cím", required=False)
+    phone_number = forms.CharField(max_length=12, label="Telefonszám", required=False)
 
     class Meta:
         model = User
         fields = ['username', 'email']
-        labels = {
-            'username': 'Felhasználónév',
-            'email': 'E-mail cím'
-        }
-        help_texts = {
-            'username': 'Csak betűk, számok és @/./+/-/_ karakterek engedélyezettek.'
-        }
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.get('instance')
         super().__init__(*args, **kwargs)
-
-        if user and hasattr(user, 'userinfo'):
-            self.fields['address'].initial = user.userinfo.address
-            self.fields['birth_date'].initial = user.userinfo.birth_date
-            self.fields['phone_number'].initial = user.userinfo.phone_number
+        user = self.instance
+        if hasattr(user, 'phoneshop_user'):
+            self.fields['address'].initial = user.phoneshop_user.address
+            self.fields['phone_number'].initial = user.phoneshop_user.phone_number
 
     def save(self, commit=True):
-        user = super().save(commit=False)
-        user_info, created = UserInfo.objects.get_or_create(user=user)
-        user_info.address = self.cleaned_data.get('address')
-        user_info.birth_date = self.cleaned_data.get('birth_date')
-        user_info.phone_number = self.cleaned_data.get('phone_number')
+        user = super().save(commit=commit)
+        user_info, _ = UserInfo.objects.get_or_create(user=user)
+        address = self.cleaned_data.get('address')
+        if address is not None and address.strip() != '':
+            user_info.address = address
+
+        phone = self.cleaned_data.get('phone_number')
+        if phone is not None and phone.strip() != '':
+            user_info.phone_number = phone
 
         if commit:
-            user.save()
             user_info.save()
+
         return user
