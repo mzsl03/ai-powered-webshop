6ede79987d91e4fb7d173c26d9779a74e3eaf649
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-09 17:26:56 +0100

    fix: user id is not passed to url, to avoid misuse

--- core_site/application/urls.py
+++ core_site/application/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='forgot_password/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='forgot_password/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='forgot_password/password_reset_complete.html'), name='password_reset_complete'),
-    path('orders/<int:user_id>/', views.user_order, name='user_order'),
-    path('orders/', views.all_user_order, name='all_user_order'),
+    path('orders/', views.user_order, name='user_order'),
+    path('orders_all/', views.all_user_order, name='all_user_order'),
+    path("checkout/", views.checkout, name="checkout"),
 ]
