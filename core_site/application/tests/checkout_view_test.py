e83a9ae79984d0180c59040fe3e3b06e26ca7c7e
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-12-07 08:46:17 +0100

    test: add test case setup

--- core_site/application/tests/checkout_view_test.py
+++ core_site/application/tests/checkout_view_test.py
@@ -30,3 +30,24 @@ class OrdersMock:
 
 class OrderItemMock:
     pass
+
+
+class CheckoutViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.checkout_url = reverse('checkout')
+        self.cart_url = reverse('cart')
+        self.user_order_url = reverse('user_order')
+        self.login_required_redirect = '/'
+
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+        self.product_a = ProductMock(name="Product A", price=5000)
+
+        self.cart_items_data = [
+            CartItemMock(product=self.product_a, quantity=2, price=5000, color="Red"),  # Összesen 10000
+        ]
