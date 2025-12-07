09c9267bb073fa4631bb6eb244348525122d2698
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-12-07 08:27:57 +0100

    test: add mocker to chatbot test

---
+++ core_site/application/tests/ai_chat_bot_test.py
@@ -0,0 +1,12 @@
+class ProductMock:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        self._price = value
